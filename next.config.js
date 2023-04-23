71186a221d406241743ed120249f512805c7b82b
sushidesu <hugemerge@gmail.com>
2023-04-23 16:03:44 +0900

    use mdx as page

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ const withMDX = require("@next/mdx")()
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  pageExtensions: ["tsx", "mdx"],
   experimental: {
     appDir: true,
   },
---
+++ src/app/about/layout.tsx
@@ -0,0 +1,5 @@
+import styles from "./layout.module.css"
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return <main className={styles.main}>{children}</main>
+}
